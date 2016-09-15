07b6a101d52b5fd7e1cfdadce05ba40ac0ea46df
Vincent Sanders <vince@kyllikki.org>
2016-09-16 00:42:22 +0100

    improve header doccomment

--- include/nspsl.h
+++ include/nspsl.h
@@ -7,4 +7,11 @@
  *                http://www.opensource.org/licenses/mit-license.php
  */
 
+/**
+ * get the public suffix of a hostname
+ *
+ * \param hostname The name to check
+ * \return pointer to the public suffix of the hostname if it was valid else
+ * NULL.
+ */
 const char *nspsl_getpublicsuffix(const char *hostname);
